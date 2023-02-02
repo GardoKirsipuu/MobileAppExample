bfa931fe317510108d83e511660bdc493afaf2d5
Gardo <gardo1999@gmail.com>
2023-02-02 11:15:00 +0200

    Created splash screen

---
+++ src/components/Button/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+import { styles } from "./styles";
+
+const Button = ({ title }) => {
+    const handlePress = () => {
+        console.log('Button pressed');
+    }
+
+    return (
+        <TouchableOpacity activeOpacity={0.6} onPress={handlePress} style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+export default Button;
