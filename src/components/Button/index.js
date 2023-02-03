999ae8ab94f962c9590315bdd84526129b2c7deb
Gardo <gardo1999@gmail.com>
2023-02-03 11:22:26 +0200

    Created authorization screens

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,13 +3,9 @@ import { TouchableOpacity, Text } from "react-native";
 
 import { styles } from "./styles";
 
-const Button = ({ title }) => {
-    const handlePress = () => {
-        console.log('Button pressed');
-    }
-
+const Button = ({ title, onPress, style }) => {
     return (
-        <TouchableOpacity activeOpacity={0.6} onPress={handlePress} style={styles.container}>
+        <TouchableOpacity activeOpacity={0.6} onPress={onPress} style={[styles.container, style]}>
             <Text style={styles.title}>{title}</Text>
         </TouchableOpacity>
     );
