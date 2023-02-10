d92c778d9b2b6ad47e6c75dc59adcf4b62902d04
Gardo <gardo1999@gmail.com>
2023-02-10 10:47:13 +0200

    Created navigation between screens and tabs

--- src/screens/auth/Signin/index.js
+++ src/screens/auth/Signin/index.js
@@ -8,19 +8,27 @@ import Separator from "../../../components/Separator";
 import GoogleLogin from "../../../components/GoogleLogin";
 import { styles } from "./styles";
 
-const Signin = () => {
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Signin = ({navigation}) => {
     const [checked, setChecked] = useState(false);
 
+    const onBack = () => {
+        navigation.goBack();
+    }
+
     return (
-        <View style={styles.container}>
-            <AuthHeader title="Sign In" />
-            <Input label="Email" placeholder="example@gmail.com" />
-            <Input isPassword label="Password" placeholder="********" />
-            <Button style={styles.button} onPress={() => console.log('Clicked')} title="Sign Up" />
-            <Separator text="Or sign in with" />
-            <GoogleLogin />
-            <Text style={styles.footerText}>Don't have an account? <Text style={styles.footerLink}>Sign Up</Text></Text>
-        </View>
+        <SafeAreaView>
+            <View style={styles.container}>
+                <AuthHeader onBackPress={onBack} title="Sign In" />
+                <Input label="Email" placeholder="example@gmail.com" />
+                <Input isPassword label="Password" placeholder="********" />
+                <Button style={styles.button} onPress={() => console.log('Clicked')} title="Sign Up" />
+                <Separator text="Or sign in with" />
+                <GoogleLogin />
+                <Text style={styles.footerText}>Don't have an account? <Text style={styles.footerLink}>Sign Up</Text></Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -3,7 +3,17 @@ import { Text, View, Image, Pressable } from "react-native";
 import Button from "../../../components/Button";
 import { styles } from "./styles";
 
-const Splash = () => {
+const Splash = ({navigation}) => {
+    console.log('navigation => ', navigation)
+
+    const onSignUp = () => {
+        navigation.navigate('Signup')
+    }
+
+    const onSignIn = () => {
+        navigation.navigate('Signin')
+    }
+
     return (
         <View style={styles.container}>
             <Image resizeMode="contain" style={styles.image} source={require('../../../assets/splash_image.png')}/>
@@ -14,9 +24,9 @@ const Splash = () => {
                 <Text style={styles.title}>Here!</Text>
             </View>
 
-            <Button title="Sign Up" />
+            <Button onPress={onSignUp} title="Sign Up" />
 
-            <Pressable hitSlop={20}>
+            <Pressable onPress={onSignIn} hitSlop={20}>
                 <Text style={styles.footerText}>Sign In</Text>
             </Pressable>
         </View>
