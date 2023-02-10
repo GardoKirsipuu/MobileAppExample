91fb4c54f20b470d20a9cc8b82c57ac1f4e9a2e6
Gardo <gardo1999@gmail.com>
2023-02-10 13:02:32 +0200

    Created homepage screen

--- src/screens/app/Home/styles.js
+++ src/screens/app/Home/styles.js
@@ -4,6 +4,9 @@ import { colors } from "../../../utils/colors";
 
 export const styles = StyleSheet.create({
     container: {
-
+        padding: 16
+    },
+    list: {
+        paddingVertical: 24
     }
 })
